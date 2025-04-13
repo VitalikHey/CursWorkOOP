83540c8ed3041e3791bfa3e71219c3bec2d69d4f
Курбанов Виталий <kurbanov.vr@reliab.tech>
2025-04-13 13:02:04 +0300

    Поменял названии, добавил комиссию в транзакцию

--- library/converter/transaction.h
+++ library/converter/transaction.h
@@ -1,17 +1,21 @@
-#ifndef CURSWORKOOP_CONVERTER_H
-#define CURSWORKOOP_CONVERTER_H
+#ifndef CURSWORKOOP_TRANSACTION_H
+#define CURSWORKOOP_TRANSACTION_H
 
 #include <map>
 #include <stdexcept>
 #include <limits>
 #include "../saving/saving.h"
 
-class Converter {
+class Transaction {
 private:
     std::map<std::string, Saving> m_currencies;
-
+    int m_rateToDollarSentSaving{};
+    int m_rateToDollarReceivedSaving{};
+    double m_resultTransaction{};
+    double m_sentSaving{};
+    double m_receivedSaving{};
 public:
-    Converter() = default;
+    Transaction() = default;
 
     void addCurrency(const Saving &currency) {
         if (m_currencies.count(currency.getName()) > 0) {
@@ -28,18 +32,26 @@ public:
         return it->second;
     }
 
-    double convert(double amount, const std::string &fromCurrencyName, const std::string &toCurrencyName) const {
+    double conductTransaction(double amount, const std::string &fromCurrencyName, const std::string &toCurrencyName, const double commission) const {
         const Saving &fromCurrency = getCurrency(fromCurrencyName);
         const Saving &toCurrency = getCurrency(toCurrencyName);
 
         if (fromCurrency.getRateToDollar() == 0.0) {
             throw std::runtime_error("Невозможно конвертировать из валюты с нулевым курсом.");
         }
+
         if (toCurrency.getRateToDollar() == 0.0) {
             throw std::runtime_error("Невозможно конвертировать в валюту с нулевым курсом.");
         }
 
         double amountInDollars = amount * fromCurrency.getRateToDollar();
+
+        // Вычисляем комиссию
+        double fee = amountInDollars * commission;
+
+        // Вычитаем комиссию из суммы в долларах
+        amountInDollars -= fee;
+
 
         return amountInDollars / toCurrency.getRateToDollar();
     }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include "library/cryptocurrency/cryptocurrency.h"
 #include "library/currency/currency.h"
-#include "library/converter/converter.h"
+#include "library/converter/transaction.h"
 
 int main() {
-    Converter converter;
+    Transaction converter;
 
     Currency usd;
     usd.setName("USD");
@@ -16,15 +16,15 @@ int main() {
 
     Currency rub;
     rub.setName("RUB");
-    rub.setRateToDollar(0.013);
+    rub.setRateToDollar(0.011);
 
     converter.addCurrency(usd);
     converter.addCurrency(eur);
     converter.addCurrency(rub);
 
     try {
-        double dollars = converter.convert(100.0, "EUR", "USD");
-        std::cout << "100 EUR = " << dollars << " USD" << std::endl;
+        double dollars = converter.conductTransaction(91, "RUB", "USD", 0.05);
+        std::cout << "91 RUB = " << dollars << " USD" << std::endl;
     } catch (const std::exception &e) {
         std::cerr << "Ошибка: " << e.what() << std::endl;
     }
