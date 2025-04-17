171edf07349fa4d57efc5871d48305d6f0c57040
Курбанов Виталий <kurbanov.vr@reliab.tech>
2025-04-17 15:33:52 +0300

    Добавил тестов

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include "../library/user-interface/user-interface.h"
+#include "../library/test/test.h"
+#include <windows.h>
 
 int main() {
+    SetConsoleOutputCP(CP_UTF8);
+
+    std::cout << "Протестировать программу перед использованием? (y/n): ";
+    std::string testChoice;
+    std::getline(std::cin, testChoice);
+
+    if (testChoice == "y" || testChoice == "Y") {
+        bool testsPassed = ConverterTest::test();
+        if (!testsPassed) {
+            std::cout << "Тесты не пройдены. Запуск программы отменен." << std::endl;
+            return 1;
+        }
+        std::cout << "Тесты пройдены. Приятного использования программы." << std::endl;
+    } else {
+        std::cout << "Пропуск тестирования." << std::endl;
+    }
+
     UserInterface userInterface;
     userInterface.run();
-    return  0;
+
+    return 0;
 }
